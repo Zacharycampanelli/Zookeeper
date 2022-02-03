f9f432bfb7a3a8835258ca7ba4d6f044524c1e19
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-02-02 19:04:08 -0500

    front end files added

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -111,6 +112,10 @@ app.post('/api/animals', (req, res) => {
     const animal = createNewAnimal(req.body, animals);
     res.json(animals);
   }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
 });
 
 app.listen(PORT, () => {
