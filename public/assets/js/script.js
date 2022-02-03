c03e6e1082ff3beb66219b0cf8703429b628b7aa
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-02-02 20:03:37 -0500

    additional GET and POST requests

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,11 +1,51 @@
-const $animalForm = document.querySelector('#animal-form');
+const $animalForm = document.querySelector('#animals-form');
+const $displayArea = document.querySelector('#display-area');
 
-const handleAnimalFormSubmit = event => {
+const printResults = resultArr => {
+  console.log(resultArr);
+
+  const animalHTML = resultArr.map(({ id, name, personalityTraits, species, diet }) => {
+    return `
+  <div class="col-12 col-md-5 mb-3">
+    <div class="card p-3" data-id=${id}>
+      <h4 class="text-primary">${name}</h4>
+      <p>Species: ${species.substring(0, 1).toUpperCase() + species.substring(1)}<br/>
+      Diet: ${diet.substring(0, 1).toUpperCase() + diet.substring(1)}<br/>
+      Personality Traits: ${personalityTraits
+        .map(trait => `${trait.substring(0, 1).toUpperCase() + trait.substring(1)}`)
+        .join(', ')}</p>
+    </div>
+  </div>
+    `;
+  });
+
+  $displayArea.innerHTML = animalHTML.join('');
+};
+
+const getAnimals = (formData = {}) => {
+  let queryUrl = '/api/animals?';
+
+  Object.entries(formData).forEach(([key, value]) => {
+    queryUrl += `${key}=${value}&`;
+  });
+
+  console.log(queryUrl);
+
+  fetch(queryUrl)
+    .then(response => {
+      if (!response.ok) {
+        return alert('Error: ' + response.statusText);
+      }
+      return response.json();
+    })
+    .then(animalData => {
+      console.log(animalData);
+      printResults(animalData);
+    });
+};
+
+const handleGetAnimalsSubmit = event => {
   event.preventDefault();
-
-  // get animal data and organize it
-  const name = $animalForm.querySelector('[name="animal-name"]').value;
-  const species = $animalForm.querySelector('[name="species"]').value;
   const dietRadioHTML = $animalForm.querySelectorAll('[name="diet"]');
   let diet;
 
@@ -19,13 +59,20 @@ const handleAnimalFormSubmit = event => {
     diet = '';
   }
 
+  const personalityTraitArr = [];
   const selectedTraits = $animalForm.querySelector('[name="personality"').selectedOptions;
-  const personalityTraits = [];
-  for (let i = 0; i < selectedTraits.length; i += 1) {
-    personalityTraits.push(selectedTraits[i].value);
-  }
-  const animalObject = { name, species, diet, personalityTraits };
 
+  for (let i = 0; i < selectedTraits.length; i += 1) {
+    personalityTraitArr.push(selectedTraits[i].value);
+  }
+
+  const personalityTraits = personalityTraitArr.join(',');
+
+  const animalObject = { diet, personalityTraits };
+
+  getAnimals(animalObject);
 };
 
-$animalForm.addEventListener('submit', handleAnimalFormSubmit);
+$animalForm.addEventListener('submit', handleGetAnimalsSubmit);
+
+getAnimals();
